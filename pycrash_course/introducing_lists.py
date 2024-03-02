4ebe1070a878e1db5e7fd2e405ba88c65edd2b64
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-03-02 13:25:06 +0000

    Complete exercise 3.6

--- pycrash_course/introducing_lists.py
+++ pycrash_course/introducing_lists.py
@@ -37,3 +37,69 @@ print(f'Dear {guest_list[0]}, please come to my party.')
 print(f'Dear {guest_list[1]}, please come to my party.')
 print(f'Dear {guest_list[2]}, please come to my party.')
 print(f'Dear {guest_list[3]}, please come to my party.')
+
+#You just heard that one of your guests can't make the dinner, so you need to send out a new set of invitations. You'll have to think of someone else to invite.'
+print('\n3.5 Changing Guest List\n- - - - - -')
+
+guest_list = ['Cookie Monster', 'Bert', 'Ernie', 'Forest Gump']
+print(f'Dear {guest_list[0]}, please come to my party.')
+print(f'Dear {guest_list[1]}, please come to my party.')
+print(f'Dear {guest_list[2]}, please come to my party.')
+print(f'Dear {guest_list[3]}, please come to my party.')
+
+#add a print at the end of the last program stating the name of the guest who can't make it.
+print(f'\nOh dear, it looks like {guest_list[3]} is busy and can\'t make it. That\'s a shame.')
+guest_list[3] = 'The Count (ah ah ah ...)'
+print(guest_list)
+
+#Print a secocnd set of invitation messages, one for each person who is still in your list.
+print(f'Dear {guest_list[0]}, please come to my party.')
+print(f'Dear {guest_list[1]}, please come to my party.')
+print(f'Dear {guest_list[2]}, please come to my party.')
+print(f'Dear {guest_list[3]}, please come to my party.')
+
+#3.6 You just found a bigger dinner table, so now more space is available, Think of three more guests to invite to dinner.
+print('\n3.6 More Guests\n- - - - - -')
+#Start with your program from previous eercise. Add a print() call to the end of your program, informing people that you found a bigger table.
+print(f'Hey {guest_list[0]}, I\'ve found a bigger table so we can have a bigger party! Awesome.')
+print(f'Hey {guest_list[1]}, I\'ve found a bigger table so we can have a bigger party! Awesome.')
+print(f'Hey {guest_list[2]}, I\'ve found a bigger table so we can have a bigger party! Awesome.')
+print(f'Hey {guest_list[3]}, I\'ve found a bigger table so we can have a bigger party! Awesome.')
+#Use insert() to add one new guest to the beginning of your list.``
+guest_list.insert(0, 'Danger Mouse')
+print(guest_list)
+#Use insert() to add one new guest list to the middle of your list.
+guest_list.insert(2, 'Rick')
+print(guest_list)
+#Use append() to add a new guest to the end of your list.
+guest_list.append('Dwayne "The Rock" Johnson')
+print(guest_list)
+#Print out a new set of invitations, one for each guest.
+print(f'Hey {guest_list[0]}, please will you still come to me biggerer and betterer party?')
+print(f'Hey {guest_list[1]}, please will you still come to me biggerer and betterer party?')
+print(f'Hey {guest_list[2]}, please will you still come to me biggerer and betterer party?')
+print(f'Hey {guest_list[3]}, please will you still come to me biggerer and betterer party?')
+print(f'Hey {guest_list[4]}, please will you still come to me biggerer and betterer party?')
+print(f'Hey {guest_list[5]}, please will you still come to me biggerer and betterer party?')
+print(f'Hey {guest_list[6]}, please will you still come to me biggerer and betterer party?')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
