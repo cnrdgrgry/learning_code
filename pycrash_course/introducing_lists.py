030459faa66c8033d083a726c8217177888c9d0e
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-03-02 12:29:27 +0000

    Continue with list exercises
    Complete exerceses 3.3, 3.4

--- pycrash_course/introducing_lists.py
+++ pycrash_course/introducing_lists.py
@@ -21,3 +21,19 @@ print(names[2] +'! Get off the floor!')
 print('Good afternoon '+ names[-1] + '.')
 
 #3.3 Your Own List: Think of your favourite mode of transportation, and make a list that stores several examples. use your list to print a serires of statements about these items, such as 'I would like to own a ...'
+print('\n3.3 Your Own List\n- - - - - -')
+
+my_list = ['Grappler', '520', 'ISO', 'Krampus', 'Stache']
+print('my list = ' + str(my_list))
+message = f'my first bicycle was a {my_list[1]}, gravel bike.'
+print(message)
+print(f'I\'d really like a {my_list[-3]}, but I\'d settle for an {my_list[0]}.')
+
+#If youu could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you'd like to invite to dinner. Then use your list to print a message to each person, inviting them to dinner.'
+print('\n3.4 Guest List\n- - - - - -')
+guest_list = ['Cookie Monster', 'Bert', 'Ernie', 'Forest Gump']
+
+print(f'Dear {guest_list[0]}, please come to my party.')
+print(f'Dear {guest_list[1]}, please come to my party.')
+print(f'Dear {guest_list[2]}, please come to my party.')
+print(f'Dear {guest_list[3]}, please come to my party.')
