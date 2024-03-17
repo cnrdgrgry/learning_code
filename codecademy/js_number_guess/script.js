32219875222251bea702baadd2b816a975686de2
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-03-17 16:30:52 +0000

    Create a compareGuesses() function
    This function:
      Has three aprameters representing the user, computer guese and secret
    target (created by random generagetion function).
      Determins which palyer wins based on which guess is closest. If the
    answer is tied, the human should win
      Returns 'true' if the human wins, 'false' for the computer

--- codecademy/js_number_guess/script.js
+++ codecademy/js_number_guess/script.js
@@ -4,3 +4,45 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+//Create a function to return a random integer between 0 and 9 to be called at start of each
+//new round.
+
+let generateTarget = () => Math.floor(Math.random() * 10);
+
+//Create a function that has three params; human, comp and secret target.
+//It determins which player wins or a tie
+//returns 'true' for human win, 'false' for comp win.
+let secretTarget = generateTarget();
+console.log(secretTarget);
+
+let compareGuesses = (human, computer, secretTarget) => {
+
+  const humanDiff = (human, secretTarget) => {
+    if (human > secretTarget) {
+      return human - secretTarget;
+    } else {
+      return secretTarget - human;
+    }
+  } 
+
+  const compDiff = (computer, secretTarget) => {
+    if (computer > secretTarget) {
+      return computer - secretTarget;
+    } else {
+      return secretTarget - computer;
+    }
+  }
+
+  const humanDifference = humanDiff(human, secretTarget);
+  const compDifference = compDiff(computer, secretTarget);
+  
+   if (humanDifference === compDifference) {
+     return true;
+   } else if (humanDifference < compDifference) {
+     return true;
+   } else {
+     return false;
+   }
+}
+console.log(compareGuesses(5, 6, secretTarget));
+
