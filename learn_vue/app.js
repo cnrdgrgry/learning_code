c152c83bed71fa51b21a19a8b44de8c3b90121ed
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-09-29 18:30:51 +0100

    Complete lesson2 : the basics

--- learn_vue/app.js
+++ learn_vue/app.js
@@ -3,7 +3,16 @@ const FlashWord = {
 		return {
 			wordA: "hola",
 			wordB: "hello",
+			answer: "",
+			correct: null,
+			showFeedback: null,
 		};
+	},
+	methods: {
+		checkAnswer() {
+			this.correct = this.wordB == this.answer;
+			this.showFeedback = true;
+		},
 	},
 };
 
