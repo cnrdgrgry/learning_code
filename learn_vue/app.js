c7693d16724be9481b6465820c9e174704b9d641
Conrad G <143351775+cnrdgrgry@users.noreply.github.com>
2024-10-01 16:01:27 +0100

    Experiment with Form input binding Implement check, radio, select and textArea

--- learn_vue/app.js
+++ learn_vue/app.js
@@ -6,15 +6,46 @@ const FlashWord = {
 			answer: "",
 			correct: null,
 			showFeedback: null,
+			image: null,
+			hasError: false,
+			showHint: false,
+			categories: ["greetings"],
+			level: "moderate",
+			textContent: "",
+
+			// Array example
+			spanishWords: ["hola", "adios", "uno", "dos"],
+
+			// Object example
+			word: { a: "hola", b: "hello" },
+
+			// Array of objects example
+			words: [
+				{ wordA: "hola", wordB: "hello" },
+				{ wordA: "adios", wordB: "goodbye" },
+				{ wordA: "uno", wordB: "one" },
+				{ wordA: "dos", wordB: "two" },
+			],
 		};
 	},
 	methods: {
 		checkAnswer() {
+			if (this.answer == "") {
+				this.hasError = true;
+				return;
+			}
+			this.hasError = false;
 			this.correct = this.wordB == this.answer;
+			if (this.correct) {
+				this.image =
+					"https://s3.amazonaws.com/codewithsusan/circle-correct@2x.png";
+			} else {
+				this.image =
+					"https://s3.amazonaws.com/codewithsusan/circle-incorrect@2x.png";
+			}
 			this.showFeedback = true;
 		},
 		resetForm() {
-			this.correct = null;
 			this.answer = "";
 			this.showFeedback = null;
 		},
